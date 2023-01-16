a03622077c63a15993cfc309fbd5e6fd80564ddb
citrovin <94009181+citrovin@users.noreply.github.com>
2023-01-16 11:33:23 +0100

    Fucking whisper is giving me errors again. I am fed up with this model

--- whisper/whisper-v2.py
+++ whisper/whisper-v2.py
@@ -29,12 +29,6 @@ mic = pyaudio.PyAudio()
 stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
 
-
-
-
-
-#output = pipe(inputs=path)
-#print(output)
 frames=[]
 try:
     while 1:
