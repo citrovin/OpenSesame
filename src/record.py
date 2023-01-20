830a6f22bdb027fb8a7cbd82298ae7d50963d6e3
Philipp Ahrendt <Pipo@Philipps-MacBook-Pro.local>
2023-01-20 21:31:04 +0100

    Fix record.py

--- src/record.py
+++ src/record.py
@@ -20,23 +20,23 @@ def record_audio(filename):
         f.write(audio.get_wav_data())
 
 
-path = '../data/'
+data_path = '../data/'
 
 time = datetime.now().strftime("%d_%m_%H_%M_%S")
 
 while True:
     choice=int(input("\n 1.Record audio for positive training sample \n 2.Record audio for negative training sample\n 1.Record audio for positive validation sample \n 2.Record audio for negative validation sample\n"))
-
+    file_path = data_path
     if(choice==1):
-        path += 'train/positive/pos_'
+        file_path += 'train/positive/pos_'
     elif(choice==2):
-        path += 'train/negative/neg_'
+        file_path += 'train/negative/neg_'
     elif(choice==3):
-        path += 'validation/positive/pos_'
+        file_path += 'validation/positive/pos_'
     elif(choice==4):
-        path += 'validation/negative/neg_'
+        file_path += 'validation/negative/neg_'
 
-    filename = path + time + '.wav'
+    filename = file_path + time + '.wav'
 
     # Record audio samples
     record_audio(filename)
